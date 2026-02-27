c066be0189dc66cc80030fe19dc666ab847b0428
Santiago Lucas <santiagoandresramirezlucas@gmail.com>
2026-02-27 16:18:15 -0500

    first commit

---
+++ src/Comandos_Git.java
@@ -0,0 +1,12 @@
+public class Comandos_Git {
+    public static void main(String[] args){
+    int nota = 5;
+        switch (nota){
+            case 5:
+                System.out.println("Aprobado");
+            case 1:
+                System.out.println("Reprobado");
+        }
+
+    }
+}
