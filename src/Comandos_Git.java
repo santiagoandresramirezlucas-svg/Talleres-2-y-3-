aed22e6dc4de9d3edf36a55d66a2a90a3359099c
Santiago Lucas <santiagoandresramirezlucas@gmail.com>
2026-02-27 18:00:38 -0500

    olpmejE

--- src/Comandos_Git.java
+++ src/Comandos_Git.java
@@ -1,11 +1,15 @@
 public class Comandos_Git {
     public static void main(String[] args){
-    int nota = 5;
-        switch (nota){
+        int nota = 5;
+        switch (nota) {
             case 5:
                 System.out.println("Aprobado");
             case 1:
                 System.out.println("Reprobado");
+
+
+
+
         }
     }
 }
